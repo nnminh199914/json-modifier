5cc637f5b057bb480b0f7a2f48c30396f62c3613
nnminh199914 <30563683+nnminh199914@users.noreply.github.com>
2023-10-11 17:13:30 +0200

    json
    dễ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { applyValue, JsonViewer } from '@textea/json-viewer';
+import { useCallback, useEffect, useState } from 'react';
 
-function App() {
+const example = {
+  string: 'Lorem ipsum dolor sit amet',
+  integer: 42,
+  float: 114.514,
+  bigint: 10086n,
+  null: null,
+  undefined,
+  timer: 0,
+  date: new Date('Tue Sep 13 2022 14:07:44 GMT-0500 (Central Daylight Time)'),
+};
+
+const App = () => {
+  const [src, setSrc] = useState(() => example);
+
+  const handleChange = useCallback((path, oldValue, newValue) => {
+    setSrc((src) => applyValue(src, path, newValue));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+      <JsonViewer
+        style={{ fontSize: "20px" }}
+        value={src}
+        editable={true}
+        theme={"auto"}
+        indentWidth={20}
+        highlightUpdates={true}
+        onChange={handleChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
